506c1b29bc5c3d5e7fbf922e37b17197349d82a5
Ertuğrul Çakıcı <72172226+ertugrulcakici@users.noreply.github.com>
2023-07-25 23:30:32 +0300

    ios error fixed

---
+++ lib/view/main/home/home_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return Column(
+      children: [
+        _map(),
+      ],
+    );
+  }
+
+  Widget _map() {
+    return const SizedBox();
+  }
+}
